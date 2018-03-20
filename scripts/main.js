56e358013a9c92edd87e4a9050dd5ef81ad7f7d5
Ferperemero86 <fernandoperezmerono@gmail.com>
2018-03-20 20:18:21 +0000

    Change structure and functionality (MOV)

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,116 @@
 
 $(document).ready(function() {
 
+     /*<----- Main ----->*/
 
-      var cats = ['cat1','cat2','cat3','cat4','cat5'];
+  var model = {
+      currentCat: null,
+      cats: [
+      {
+        clicks: 0,
+        name: 'cat1',
+        img: 'images/cat1.jpg'
+      },
+      {
+        clicks: 0,
+        name: 'cat2',
+        img: 'images/cat2.jpg'
+      },
+      {
+        clicks: 0,
+        name: 'cat3',
+        img: 'images/cat3.jpg'
+      },
+      {
+        clicks: 0,
+        name: 'cat4',
+        img: 'images/cat4.jpg'
+      },
+      {
+        clicks: 0,
+        name: 'cat5',
+        img: 'images/cat5.jpg'
+      }
+
+     ]
+
+  };
+
+  /*<----- Organization ----->*/
+
+  var organization = {
+
+   getCatList: function() {
+    return  model.cats;
+   },
+
+   setCurrentCat: function(cat) {
+     model.currentCat = cat;
+   },
+
+   getCurrentCat: function() {
+     return model.currentCat;
+   },
+
+   addClicks: function() {
+     model.currentCat.clicks++
+
+   },
+   getClicks: function() {
+     return model.currentCat.clicks;
+   }
+
+  };
+
+  /*<----- View ----->*/
+
+  var catList = organization.getCatList();
+
+  // Add cat names list on top of the page
+
+  for(var i = 0; i < catList.length; i++) {
+     $('#cat-list').append('<li>'+catList[i].name+'</li>');
+  }
+
+  // Add the first cat details before any click
+
+  organization.setCurrentCat(catList[0]);
+  var currentCat = organization.getCurrentCat();
+  $('#name').append(currentCat.name);
+  $('#clicks').append(currentCat.clicks);
+  $('#cat-image').attr('src',currentCat.img);
+
+  // Add current cat details when click a cat name on top
+
+  $('#cat-list li').click(function(e) {
+    for(var i = 0; i < catList.length; i++) {
+     if(catList[i].name === e.target.innerHTML) {
+       organization.setCurrentCat(catList[i]);
+     }
+   }
+    var getCat = organization.getCurrentCat();
+
+    $('#name').html('');
+    $('#clicks').html('');
+    $('#clicks').append(getCat.clicks);
+    $('#cat-image').attr('src','');
+
+    $('#name').append(getCat.name);
+    $('#cat-image').attr('src',getCat.img);
 
 
-      $.each(cats, function(index,cat) {
-        var clicks = 0;
+  });
 
-        $('.first-section ul').append(`<li class='${cat}'>${cat}</li>`);
+  //Add and Increade clicks for each cat
 
+  $('#cat-image').click(function() {
+    organization.addClicks();
 
-        $(`li.${cat}`).click(function() {
-           $('.first-section div').remove()
-           $('.first-section').append(`<div class =${cat}><span class='name'>${cat}</span><span>clicks</span><span class='counter'></span>
-                                      <image src='images/${cat}.jpg' class=${cat}></div>`);
-           $(`.${cat} .counter`).append(`<span>${clicks}</span>`);
+    $('#clicks').html('');
+    var getClicks = organization.getClicks();
+    $('#clicks').append(getClicks);
 
-          $(`img.${cat}`).click(function() {
-             $(`div.${cat} .counter span`).remove();
-             clicks++;
-             $(`.${cat} .counter`).append(`<span>${clicks}</span>`);
-          });
+  });
 
-        });
-
-
-       });
 
 });//End ready
